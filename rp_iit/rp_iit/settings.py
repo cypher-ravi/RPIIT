910058289a93aff0930fb8508f5c50797026266e
Ravi <ronniloreo@gmail.com>
2020-12-24 19:51:49 -0500

    remove pagination

--- rp_iit/rp_iit/settings.py
+++ rp_iit/rp_iit/settings.py
@@ -156,8 +156,6 @@ REST_FRAMEWORK = {
         # 'rest_framework.permissions.IsAuthenticated',
         
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
 
