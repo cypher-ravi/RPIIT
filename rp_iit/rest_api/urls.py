e308c9152ee4aa7bf6fdceacfc3ede29b8bbfd77
Ravi <ronniloreo@gmail.com>
2020-12-28 20:38:51 -0500

    added list of cul,social,sport apis and update img url

--- rp_iit/rest_api/urls.py
+++ rp_iit/rest_api/urls.py
@@ -14,11 +14,15 @@ urlpatterns = [
     path('student_profile/', StudentProfileView.as_view()),
     path('update_student_profile/<str:pk>', UpdateStudentProfileView.as_view()),
 
-    path('student_corner/<str:pk>', StudentDetailsView.as_view()),
+    path('student_detail/<str:pk>', StudentDetailsView.as_view()),
     path('student_sports_detail/<str:pk>', SportsDetailOfStudentView.as_view()),
     path('student_cultural_activity_detail/<str:pk>', CulturalActivityDetailOfStudentView.as_view()),
+    path('student_social_activity_detail/<str:pk>', SocialActivityDetailOfStudentView.as_view()),
 
     path('list_of_companies/', ListOfCompaniesView.as_view()),
+    path('list_of_cultural_activties/', CulturalActivityList.as_view()),
+    path('list_of_social_activties/', SocialActivityList.as_view()),
+    path('list_of_sport_events/', SportEventsList.as_view()),
     
 
 
