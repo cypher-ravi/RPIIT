5ea2a6211007c1dcab8a067a1a5374fb5a524d00
cypher-ravi <ronniloreo@gmail.com>
2021-02-02 18:57:41 +0530

    added payU Kit

--- rp_iit/Payment_gateway/models.py
+++ rp_iit/Payment_gateway/models.py
@@ -1,3 +1,55 @@
 from django.db import models
-
+from authentication.models import User
 # Create your models here.
+# {'return_data': {'isConsentPayment': 
+# '0', 'mihpayid': '9084144640',
+#  'mode': 'CC',
+#  'status': 'success',
+#  'unmappedstatus': 'captured',
+#  'key': 'ZQGI2x56',
+#  'txnid': 'txna6784692-f102-4820-920d-68559264ac46',
+#  'amount': '9451.00', 
+# 'addedon': '2021-02-02 16:19:15', 
+# 'productinfo': 'Susan',
+#  'firstname': 'Alexander',
+#  'lastname': '', 'address1':
+#  '', 'address2': '1266 Laurie Fords Suite 714br /New Jason 
+#  HI 
+# 01847', 'city': '',
+#  'state': '', 
+# 'country': 'test', 
+# 'zipcode': '  
+#  ', 'email': 'janicephelps@hotmail.com',
+# 'phone': '99376444162', 
+# 'udf1': '', 
+# 'udf2': '',
+#  'udf3': '',
+#  'udf4': '',
+#  'udf5': '',
+#  'udf6': '',
+#  'udf7': '',
+#  'udf8': '',
+#  'udf9': '',
+#  'udf10': ''
+# , 'hash': 'b456b9722739191a7207696533d5488358d47f5eba704c94eb5cb7c656f3f84c23d48f809598e0307c94b5375cdd4e029f095ef92285eaff8d9259a376ce595e', 
+# 'field1': '127913664928',
+#  'field2': '369825',
+#  'field3': '833002059068314',
+# 'field4': 'eG9qMVlDSnI3VE9tekJadGpwR1U=',
+# 'field5': '05', 
+# 'field6': '',
+#  'field7': 'AUTHPOSITIVE', 'field8': '', 'field9': '', 'giftCardIssued': 'true', 'PG_TYPE': 'HDFCPG', 'encryptedPaymentId': '7ACD0801EC47B3C8D52F3E6B18151A7B', 'bank_ref_num': '833002059068314', 'bankcode': 'VISA', 'error': 'E000', 'error_Message': 'No Error', 'name_on_card': 'Test', 'cardnum': '401200XXXXXX1112', 'cardhash': 'This field is no longer supported in postback params.', 'amount_split': '{"PAYU":"9451.00"}', 'payuMoneyId': '250768997', 'discount': '0.00', 'net_amount_debit': '9451'}, 'hash_string': 'VTsYoY3kBp|success|||||||||||janicephelps@hotmail.com|Alexander|Susan|9451.00|txna6784692-f102-4820-920d-68559264ac46|ZQGI2x56', 'generated_hash': 'b456b9722739191a7207696533d5488358d47f5eba704c94eb5cb7c656f3f84c23d48f809598e0307c94b5375cdd4e029f095ef92285eaff8d9259a376ce595e', 'recived_hash': 'b456b9722739191a7207696533d5488358d47f5eba704c94eb5cb7c656f3f84c23d48f809598e0307c94b5375cdd4e029f095ef92285eaff8d9259a376ce595e', 'hash_verified': True}
+
+
+class PaymentDetail(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status=	models.CharField(max_length=9)#Transaction Status. (Described in detail in the Transaction Status section)	success;
+    firstname= models.CharField(max_length=25)
+    amount=	models.FloatField(editable = False)
+    txnid=	models.CharField(max_length=25)#Transaction Id passed by the merchant	0nf725;
+    hash=	models.TextField()
+    productinfo	= models.CharField(max_length=25)
+    mobile=	models.CharField(max_length=25)
+    email=	models.CharField(max_length=25)
+    payuMoneyId=models.CharField(max_length=25)
+    payment_mode=	models.CharField(max_length=25)
--- rp_iit/Payment_gateway/views.py
+++ rp_iit/Payment_gateway/views.py
@@ -80,18 +80,126 @@ def payu_checkout(request,**kwargs):
 
 
 # Payu success return page
+# @csrf_exempt
+# def payu_success(request):
+#     data = {k: v[0] for k, v in dict(request.POST).items()}
+#     response = payu.verify_transaction(data)
+#     print('--------------------------',response)
+
+#     return render(request, 'success.html')
+
+
+# @csrf_exempt
+# def payu_failure(request):
+#     data = {k: v[0] for k, v in dict(request.POST).items()}
+#     response = payu.verify_transaction(data)
+#     print(response)
+#     # {'return_data': {}, 'hash_string': 'VTsYoY3kBp|None|||||||||||None|None|None|None|None|None', 'generated_hash': '9691a87e776456870ac3565a24f1eedbdf50e703e030645806d7afd075acd45d9798ef6355f9345f7957ca321273d15267a78b9a2f879014cf339db2fbb3a72e', 'recived_hash': None, 'hash_verified': False}
+#     return render(request, 'failure.html')
+
+
+
+from django.http import HttpResponse,HttpResponseRedirect
+from django.template.loader import get_template
+from django.template import Context, Template,RequestContext
+import datetime
+import hashlib
+from random import randint
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
+# from django.core.context_processors import csrf
+from django.template.context_processors import csrf
+
+
+def Home(request):
+	MERCHANT_KEY = ""
+	key=""
+	SALT = ""
+	PAYU_BASE_URL = "https://sandboxsecure.payu.in/_payment"
+	action = ''
+	posted={}
+	# Merchant Key and Salt provided y the PayU.
+	for i in request.POST:
+		posted[i]=request.POST[i]
+	hash_object = hashlib.sha256(b'randint(0,20)')
+	txnid=hash_object.hexdigest()[0:20]
+	hashh = ''
+	posted['txnid']=txnid
+	hashSequence = "key|txnid|amount|productinfo|firstname|email|udf1|udf2|udf3|udf4|udf5|udf6|udf7|udf8|udf9|udf10"
+	posted['key']=key
+	hash_string=''
+	hashVarsSeq=hashSequence.split('|')
+	for i in hashVarsSeq:
+		try:
+			hash_string+=str(posted[i])
+		except Exception:
+			hash_string+=''
+		hash_string+='|'
+	hash_string+=SALT
+	hashh=hashlib.sha512(hash_string).hexdigest().lower()
+	action =PAYU_BASE_URL
+	if(posted.get("key")!=None and posted.get("txnid")!=None and posted.get("productinfo")!=None and posted.get("firstname")!=None and posted.get("email")!=None):
+		return render(request, 'current_datetime.html',{"posted":posted,"hashh":hashh,"MERCHANT_KEY":MERCHANT_KEY,"txnid":txnid,"hash_string":hash_string,"action":"https://test.payu.in/_payment" })
+	else:
+		return render(request, 'current_datetime.html',{"posted":posted,"hashh":hashh,"MERCHANT_KEY":MERCHANT_KEY,"txnid":txnid,"hash_string":hash_string,"action":"." })
+
+@csrf_protect
 @csrf_exempt
 def payu_success(request):
-    data = {k: v[0] for k, v in dict(request.POST).items()}
-    response = payu.verify_transaction(data)
-    return render(request, 'success.html')
+    c = {}
+    c.update(csrf(request))
+    status=request.POST["status"]
+    firstname=request.POST["firstname"]
+    amount=request.POST["amount"]
+    txnid=request.POST["txnid"]
+    posted_hash=request.POST["hash"]
+    key=request.POST["key"]
+    productinfo=request.POST["productinfo"]
+    email=request.POST["email"]
+    salt="GQs7yium"
+    try:
+        additionalCharges=request.POST["additionalCharges"]
+        retHashSeq=additionalCharges+'|'+salt+'|'+status+'|||||||||||'+email+'|'+firstname+'|'+productinfo+'|'+amount+'|'+txnid+'|'+key
+    except Exception:
+        retHashSeq = salt+'|'+status+'|||||||||||'+email+'|'+firstname+'|'+productinfo+'|'+amount+'|'+txnid+'|'+key
+    hashh=hashlib.sha512(retHashSeq).hexdigest().lower()
+    if(hashh !=posted_hash):
+        print("Invalid Transaction. Please try again")
+    else:
+        print("Thank You. Your order status is ", status)
+        print("Your Transaction ID for this transaction is ",txnid)
+        print("We have received a payment of Rs. ", amount ,". Your order will soon be shipped.")
+    return render(request, 'sucess.html',{"txnid":txnid,"status":status,"amount":amount})
 
 
+@csrf_protect
 @csrf_exempt
 def payu_failure(request):
-    data = {k: v[0] for k, v in dict(request.POST).items()}
-    response = payu.verify_transaction(data)
-    return JsonResponse(response)
+    c = {}
+    c.update(csrf(request))
+    status=request.POST["status"]
+    firstname=request.POST["firstname"]
+    amount=request.POST["amount"]
+    txnid=request.POST["txnid"]
+    posted_hash=request.POST["hash"]
+    key=request.POST["key"]
+    productinfo=request.POST["productinfo"]
+    email=request.POST["email"]
+    salt=""
+    try:
+        additionalCharges=request.POST["additionalCharges"]
+        retHashSeq=additionalCharges+'|'+salt+'|'+status+'|||||||||||'+email+'|'+firstname+'|'+productinfo+'|'+amount+'|'+txnid+'|'+key
+    except Exception:
+        retHashSeq = salt+'|'+status+'|||||||||||'+email+'|'+firstname+'|'+productinfo+'|'+amount+'|'+txnid+'|'+key
+    hashh=hashlib.sha512(retHashSeq).hexdigest().lower()
+    if(hashh !=posted_hash):
+        print("Invalid Transaction. Please try again")
+    else:
+        print("Thank You. Your order status is ", status)
+        print("Your Transaction ID for this transaction is ",txnid)
+        print("We have received a payment of Rs. ", amount ,". Your order will soon be shipped.")
+    return render(request, "Failure.html",{'request':request,'c':c})
+
+	
 
 
 
